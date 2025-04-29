693e7542121729a8b5767368feaa7b3320f8970e
mubashirhussan <mubashirhussan93@gmail.com>
2025-04-29 17:42:50 +0500

    title has been added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,22 +1,19 @@
-import { useNavigate } from "react-router-dom";
+import { Form, Input, Button, message } from "antd";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const [loginError, setLoginError] = useState("");
   const navigate = useNavigate();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    // Replace this with actual API authentication logic
+  const onFinish = ({ email, password }) => {
     const isAuthenticated = email === "admin@gmail.com" && password === "123";
 
     if (isAuthenticated) {
+      setLoginError(""); // clear error
       navigate("/admin-dashboard");
     } else {
-      navigate("/admin-dashboard");
-      // alert("Invalid credentials");
+      setLoginError("Invalid credentials");
     }
   };
 
@@ -34,68 +31,38 @@ export default function Login() {
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form onSubmit={handleSubmit} className="space-y-6">
-          <div>
-            <label
-              htmlFor="email"
-              className="block text-sm/6 font-medium text-gray-900"
-            >
-              Email address
-            </label>
-            <div className="mt-2">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                // required
-                autoComplete="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-              />
-            </div>
-          </div>
+        <Form layout="vertical" onFinish={onFinish} autoComplete="off">
+          <Form.Item
+            label="Email address"
+            name="email"
+            rules={[
+              { required: true, message: "Please input your email!" },
+              { type: "email", message: "Please enter a valid email!" },
+            ]}
+          >
+            <Input />
+          </Form.Item>
 
-          <div>
-            <div className="flex items-center justify-between">
-              <label
-                htmlFor="password"
-                className="block text-sm/6 font-medium text-gray-900"
-              >
-                Password
-              </label>
-              <div className="text-sm">
-                <a
-                  href="/forgot-password"
-                  className="font-semibold text-indigo-600 hover:text-indigo-500"
-                >
-                  Forgot password?
-                </a>
-              </div>
-            </div>
-            <div className="mt-2">
-              <input
-                id="password"
-                name="password"
-                type="password"
-                // required
-                autoComplete="current-password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-              />
-            </div>
-          </div>
-
-          <div>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password />
+          </Form.Item>
+          {loginError && (
+            <div className="text-red-600 text-sm mb-4">{loginError}</div>
+          )}
+          <Form.Item>
             <button
               type="submit"
               className="flex w-full cursor-pointer justify-center rounded-md !bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:!bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:!outline-indigo-600"
             >
               Sign in
             </button>
-          </div>
-        </form>
+          </Form.Item>
+        </Form>
+
         <p className="mt-10 text-center text-sm/6 text-gray-500">
           Not registered?{" "}
           <a
